4f0dd84ce4359c19fb2a5b74789c0da30f57b830
hsanguinet <huifang.sanguinet@soprasteria.com>
2021-07-28 15:47:32 +0200

    first commit

---
+++ ci/src/main/java/com/huifang/ci/CiprojetApplication.java
@@ -0,0 +1,13 @@
+package com.huifang.ci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiprojetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiprojetApplication.class, args);
+	}
+
+}
---
+++ ci/src/test/java/com/huifang/ci/CiprojetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huifang.ci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiprojetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
